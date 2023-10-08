27c9fc812ffe8d7a3aea83880b26eb8913a2ef9a
Michiyasu Uchiyama <hairstyle2004@yahoo.co.jp>
2023-10-08 16:58:06 +0900

    Update train.py

--- src/train.py
+++ src/train.py
@@ -88,8 +88,8 @@ def run(num_epochs, model_path=None):
         #train_loss = train_loss / len(SEM_train)
         print('Epoch', str(i+1), 'Train loss:', train_loss, "Train acc", train_acc)
 
-        # Validation every 5 epoch (Error code)
-        if (i+1) % 5 == 0:
+        # Validation  (Error code)
+        if False:
             val_acc, val_loss = validate_model(
                 model, SEM_val_load, criterion, i+1, True, image_save_path)
             print('Val loss:', val_loss, "val acc:", val_acc)
